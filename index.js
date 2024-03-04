a4508077a9d15b37a5ecb7057eff580acc645ae6
budi@gmail.com <mhmd.raffli09@gmail.com>
2024-03-04 22:44:54 +0700

    frist commit

---
+++ index.js
@@ -0,0 +1,28 @@
+import express from "express";
+const app = express();
+const port = 3000;
+
+app.set("view engine", "hbs");
+app.set("views", "src/view");
+
+app.use("/assets", express.static("src/assets"));
+
+app.get("/home", (req, res) => {
+  res.render("home");
+});
+
+app.get("/blog", (req, res) => {
+  res.render("blog");
+});
+
+app.get("/testi", (req, res) => {
+  res.render("testi");
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
