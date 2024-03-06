95a6f354502019b63b7e4652134fee890993c453
budi@gmail.com <mhmd.raffli09@gmail.com>
2024-03-06 20:30:46 +0700

    modifed file in this file

--- index.js
+++ index.js
@@ -12,38 +12,35 @@ app.use(express.urlencoded({ extended: false }));
 // request = dari client ke server
 // respose = dari server ke client
 
+// get = mengambil data
+// post = mengirim data
+
 // Roting
 app.get("/home", home);
 app.get("/blog", blog);
 app.get("/blogdetail/:id", blogdetail);
+app.post("/blog", handleBlog);
+
 app.get("/delete-blog/:id", handleDeleteBlog);
 app.get("/testi", testi);
 app.get("/contact", contact);
+
+const data = [];
 
 function home(req, res) {
   res.render("home");
 }
 
 function blog(req, res) {
-  const data = [
-    {
-      title: "Title 1",
-      content: "Content 1",
-    },
-
-    {
-      title: "Title 2",
-      content: "Content 2",
-    },
-  ];
-
   res.render("blog", { data });
 }
 
 function blogdetail(req, res) {
   const id = req.params.id;
 
-  res.render("blogdetail", { id });
+  res.render("blogdetail", {
+    data: [id],
+  });
 }
 
 function testi(req, res) {
@@ -54,11 +51,29 @@ function contact(req, res) {
   res.render("contact");
 }
 
+function handleBlog(req, res) {
+  // const title = req.body.title;
+  // const startDate = req.body.startDate;
+  // const endDate = req.body.endDate;
+  // const content = req.body.content;
+
+  const { title, startDate, endDate, content, techIcon } = req.body;
+
+  data.push({ title, startDate, endDate, content, techIcon });
+
+  res.redirect("/blog");
+}
+
 function handleDeleteBlog(req, res) {
-  const { id } = req.params.id;
+  const { id } = req.params;
 
-  datas.splice(id, 1);
+  // if (confirm("Apkah anda yakin ?") == true) {
+  //   data.splice(id, 1);
+  // } else {
+  //   res.redirect("/blog");
+  // }
 
+  data.splice(id, 1);
   res.redirect("/blog");
 }
 
